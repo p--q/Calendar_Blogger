396b0b204bcf70b4ce202558efa05c1db39d940b
pq <pq.blogspot@gmail.com>
2016-12-11 01:18:15 +0900

    未完成。

--- public_html/Calendar_Blogger.js
+++ public_html/Calendar_Blogger.js
@@ -1,42 +1,99 @@
 // Calendar_Bloggerモジュール
 var Calendar_Blogger = Calendar_Blogger || function() {
     var cl = {
+        callback: {
+            getArticles: function(json) {  // 指定した月のフィードを受け取るコールバック関数。
+                Array.prototype.push.apply(vars.posts, json.feed.entry);// 投稿のフィードデータを配列に追加。
+                if (json.feed.openSearch$totalResults.$t < vars.max) {  // 取得投稿数がvars.maxより小さい時はすべて取得できたと考える。
+//                    vars.posts.forEach(function(e){
+//                        var href = e.link[4].href;  // 投稿のurl
+//                        var title = e.link[4].title;  // 投稿のtitle
+//                        var pub = e.published.$t;
+//                        var up = e.updated.$t;
+//                    })
+                    
+                    createCalendar();
+                    
+                    
+                    
+                    
+                } else {  // 未取得のフィードを再取得する。
+                    var m = /(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d)\.\d\d\d(.\d\d:\d\d)/i.exec(json.feed.entry[json.feed.entry.length-1][vars.poru].$t);
+                    var dt = new Date;
+                    dt.setTime(new Date(m[1] + m[2]).getTime() - 1 * 1000);  // 1秒早める。
+                    if (vars.m==dt.getMonth()+1) {
+                        var max = vars.y + "-" + fm(vars.m) + "-" + fm(dt.getDate()) + "T" + fm(dt.getHours()) + ":" + fm(dt.getMinutes()) + ":" + fm(dt.getSeconds()) + "%2B09:00";
+                        createURL(max);                       
+                    }
+                }  
+            }
+        },
         all: function(elemID) {  // ここから開始する。
             vars.elem = document.getElementById(elemID);  
             if (vars.elem) {  
-                createCalendar();
+                var dt = new Date(2013,8,1);  // 月は-1になっている。
+                createVars(dt);
+                var max = vars.y + "-" + fm(vars.m) + "-" + fm(vars.em) + "T23:59:99%2B09:00";
+                createURL(max);
             } 
         }        
-        
     };  // end of cl
     var vars = {
-        elem : null
-        
-        
-        
+        y: null,  // 年。
+        m: null,  // 月。
+        em: null,  //月末。
+        max: 150,  // Bloggerのフィードで取得できる最大投稿数。
+        posts: [], // 投稿のフィードデータの配列。
+        poru: "published",  // publishedかupdated。
+        elem: null  // 置換するdiv要素。
     };
-    
-    
+    function createVars(dt) {
+        vars.y = dt.getFullYear();
+        vars.m = dt.getMonth() + 1; 
+        vars.em = new Date(vars.y, vars.m+1, 0).getDate();  // 月末の日付を取得。
+    }
     function createCalendar() {  // URLからラベル名と現在のページ番号を得、その後総投稿数を得るためのフィードを取得する。
-        var today = new Date(); 
         
-        
-        
-        
-//        var thisUrl = location.href;  // 現在表示しているURL。
-//        if (/\/search\/label\//i.test(thisUrl)) {  // ラベルインデックスページの場合URLからラベル名を取得。
-//            vars.postLabel = /\/search\/label\/(.+)(?=\?)/i.exec(thisUrl)[1];  // 後読みは未実装の可能性あるので使わない。
-//        } 
-//        if (!/\?q=|\.html$/i.test(thisUrl)) {  // 検索結果や固定ページではないとき。
-//            vars.currentPageNo = (/#PageNo=/i.test(thisUrl))?/#PageNo=(\d+)/i.exec(thisUrl)[1]:1;  // URLから現在のページ番号の取得。
-//            var url;  // フィードを取得するためのURL。
-//            if (vars.postLabel) {  // 総投稿数取得のためにフィードを取得するURLの作成。ラベルインデックスのときはそのラベル名の総投稿数を取得するため。
-//                url = '/feeds/posts/summary/-/' + vars.postLabel + "?alt=json-in-script&callback=PageNavi2_Blogger.callback.getTotalPostCount&max-results=1";          
-//            } else {
-//                url = "/feeds/posts/summary?max-results=1&alt=json-in-script&callback=PageNavi2_Blogger.callback.getTotalPostCount";
-//            }
-//            writeScript(url); 
-//        }
+        var re = /\d\d(?=T\d\d:\d\d:\d\d\.\d\d\d.\d\d:\d\d)/i;
+        var postdays = [];  // アンカーをつける日のデータを入れる2次元配列。
+        var aday;
+        vars.posts.forEach(function(e){
+            aday = Number(re.exec(e[vars.poru].$t));
+            postdays.push([aday, e.link[4].href, e.link[4].title]);
+        });
+
+
+
+
+        var day =  new Date(vars.y, vars.m, 1).getDay();  // 1日の曜日を取得。日曜日は0、土曜日は6になる。
+        var clNode = createElem("div");  // カレンダーを入れるdiv要素を作成。
+        clNode.style.display = "flex";
+        clNode.style.flexWrap = "wrap";
+        var dNode = createElem("div");
+        dNode.style.flexBasis = "14%";
+        dNode.style.flexGrow = "1";
+        dNode.style.textAlign = "center";
+        for(var i = 0; i < day; i++) { // 1日までの空白を取得。
+            var eNode = dNode.cloneNode(true);         
+            clNode.appendChild(eNode);
+        }
+        for(var i = 1; i < vars.em+1; i++) { 
+            var daNode = dNode.cloneNode(true);
+            
+            
+            daNode.appendChild(createElem("a"));
+            daNode.firstChild.textContent = i;  // 日付を取得。
+            
+            clNode.appendChild(daNode);
+        }
+        var s = (day+vars.em) % 7;  // 7で割ったあまりを取得。
+        if (s > 0) {  // 7で割り切れない時。
+            for(var i = 0; i < 7-s; i++) { // 月末以降の空白を取得。
+                var eNode = dNode.cloneNode(true);
+                clNode.appendChild(eNode);
+            }        
+        }
+        return clNode;
     }       
     function writeScript(url) {  // スクリプト注入。
         var ws = createElem('script');
@@ -47,8 +104,20 @@ var Calendar_Blogger = Calendar_Blogger || function() {
     function createElem(tag){  // tagの要素を作成して返す。
        return document.createElement(tag); 
     }       
-    
+    function createURL(max) { 
+        var url = "/feeds/posts/summary?alt=json-in-script&orderby=" + vars.poru + "&" + vars.poru + "-min=" + vars.y + "-" + fm(vars.m) + "-01T00:00:00%2B09:00&" + vars.poru + "-max=" + max;  // フィードを取得するためのURL。
+        url += "&callback=Calendar_Blogger.callback.getArticles&max-results=" + vars.max;
+        writeScript(url); 
+    }        
+    function fm(m) {  // 数値を2桁の固定長にする。
+        return ("0" + m).slice(-2);
+    }
+
+
+
     
     return cl;
 }();
+
+Calendar_Blogger.all("calendar_blogger");
 
