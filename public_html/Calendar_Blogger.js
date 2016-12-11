087947ae5b00adc18426f36c0cb41925f8a9498d
pq <pq.blogspot@gmail.com>
2016-12-11 23:54:58 +0900

    マウスイベントの調整。

--- public_html/Calendar_Blogger.js
+++ public_html/Calendar_Blogger.js
@@ -5,18 +5,7 @@ var Calendar_Blogger = Calendar_Blogger || function() {
             getArticles: function(json) {  // 指定した月のフィードを受け取るコールバック関数。
                 Array.prototype.push.apply(vars.posts, json.feed.entry);// 投稿のフィードデータを配列に追加。
                 if (json.feed.openSearch$totalResults.$t < vars.max) {  // 取得投稿数がvars.maxより小さい時はすべて取得できたと考える。
-//                    vars.posts.forEach(function(e){
-//                        var href = e.link[4].href;  // 投稿のurl
-//                        var title = e.link[4].title;  // 投稿のtitle
-//                        var pub = e.published.$t;
-//                        var up = e.updated.$t;
-//                    })
-                    
                     createCalendar();
-                    
-                    
-                    
-                    
                 } else {  // 未取得のフィードを再取得する。
                     var m = /(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d)\.\d\d\d(.\d\d:\d\d)/i.exec(json.feed.entry[json.feed.entry.length-1][vars.poru].$t);
                     var dt = new Date;
@@ -43,17 +32,17 @@ var Calendar_Blogger = Calendar_Blogger || function() {
         m: null,  // 月。
         em: null,  //月末。
         max: 150,  // Bloggerのフィードで取得できる最大投稿数。
-        posts: [], // 投稿のフィードデータの配列。
+        posts: [],  // 投稿のフィードデータの配列。
         poru: "published",  // publishedかupdated。
-        elem: null  // 置換するdiv要素。
+        elem: null,  // 置換するdiv要素。
+        flg: true  // ツールチップ表示のフラグ。
     };
     function createVars(dt) {
         vars.y = dt.getFullYear();
         vars.m = dt.getMonth() + 1; 
-        vars.em = new Date(vars.y, vars.m+1, 0).getDate();  // 月末の日付を取得。
+        vars.em = new Date(vars.y, vars.m-1, 0).getDate();  // 月末の日付を取得。
     }
-    function createCalendar() {  // URLからラベル名と現在のページ番号を得、その後総投稿数を得るためのフィードを取得する。
-        
+    function createCalendar() {  
         var re = /\d\d(?=T\d\d:\d\d:\d\d\.\d\d\d.\d\d:\d\d)/i;
         var dicdays = {};  // アンカーをつける日のデータを入れる辞書。
         var aday;
@@ -63,7 +52,7 @@ var Calendar_Blogger = Calendar_Blogger || function() {
             dicdays[aday].push([e.link[4].href, e.link[4].title]);   
             }
         );
-        var day =  new Date(vars.y, vars.m, 1).getDay();  // 1日の曜日を取得。日曜日は0、土曜日は6になる。
+        var day =  new Date(vars.y, vars.m-1, 1).getDay();  // 1日の曜日を取得。日曜日は0、土曜日は6になる。
         var clNode = createElem("div");  // カレンダーを入れるdiv要素を作成。
         clNode.style.display = "flex";
         clNode.style.flexWrap = "wrap";
@@ -75,21 +64,38 @@ var Calendar_Blogger = Calendar_Blogger || function() {
             var eNode = dNode.cloneNode(true);         
             clNode.appendChild(eNode);
         }
+        var daNode = dNode.cloneNode(true);
+        daNode.style.position = "relative";
+        daNode.style.display = "inline-block";
+        daNode.style.borderBottom = "1px dotted black";
+        var dsNode = createElem("span");
+        dsNode.style.visibility = "hidden";
+        dsNode.style.width = "120px";
+        dsNode.style.backgroundColor = "black";
+        dsNode.style.color = "#fff";
+        dsNode.style.textAlign = "center";
+        dsNode.style.padding = "5px 0";
+        dsNode.style.borderRradius = "6px";
+        dsNode.style.position = "absolute";
+        dsNode.style.zIndex = "1";
         for(var i = 1; i < vars.em+1; i++) { 
-            var daNode = dNode.cloneNode(true);
             if (i in dicdays) {
-                daNode.appendChild(createElem("a"));
-                daNode.firstChild.textContent = i;  // 日付を取得。
-                daNode.firstChild.href = "#";
+                var sNode = dsNode.cloneNode(true);
                 dicdays[i].forEach(function(e){
-                    daNode.firstChild.title += (daNode.firstChild.title)?"\n" + e[1]:e[1];
+                    var aNode = createElem("a");
+                    aNode.href = e[0];
+                    aNode.textContent = e[1];
+                    sNode.appendChild(aNode);         
                 });
-//                daNode.firstChild.tooltip.backgroundColor = "black";
-                
+                var Node = daNode.cloneNode(true);
+                Node.className = "tooltip";
+                Node.textContent = i;  // 日付を取得。textContentで代入すると子ノードは消えてしまう。
+                Node.appendChild(sNode);
             } else {
-                daNode.textContent = i;  // 日付を取得。
-            }
-            clNode.appendChild(daNode);
+                var Node = dNode.cloneNode(true); 
+                Node.textContent = i;  // 日付を取得。textContentで代入すると子ノードは消えてしまう。
+            } 
+            clNode.appendChild(Node);
         }
         var s = (day+vars.em) % 7;  // 7で割ったあまりを取得。
         if (s > 0) {  // 7で割り切れない時。
@@ -97,12 +103,39 @@ var Calendar_Blogger = Calendar_Blogger || function() {
                 var eNode = dNode.cloneNode(true);
                 clNode.appendChild(eNode);
             }        
-        }
-        clNode.onmouseover = popUpTitles;
-        
-        
+        } 
+        clNode.onmouseover = tooltipOn;
+        clNode.onmouseout = tooltipOff;
+        clNode.onclick = tooltip;
         vars.elem.appendChild(clNode);
-    }       
+    }
+    function tooltip(e) {
+        e=e||event; // IE sucks
+        var target = e.target||e.srcElement;  // and sucks again // target is the element that has been clicked
+        if (target && target.className=="tooltip") {
+            vars.flg = (vars.flg)?false:true;
+            target.lastChild.style.visibility = (target.lastChild.style.visibility == "hidden")?"visible":"hidden";
+            return false; // stop event from bubbling elsewhere
+        }   
+    }
+    function tooltipOn(e) {
+        e=e||event; // IE sucks
+        var target = e.target||e.srcElement;  // and sucks again // target is the element that has been clicked
+        if (target && target.className=="tooltip") {
+            target.lastChild.style.visibility = "visible";
+            return false; // stop event from bubbling elsewhere
+        }        
+    }
+    function tooltipOff(e) {
+        e=e||event; // IE sucks
+        var target = e.target||e.srcElement;  // and sucks again // target is the element that has been clicked
+        if (target && target.className=="tooltip") {
+            if (vars.flg){
+                target.lastChild.style.visibility = "hidden";
+            }
+            return false; // stop event from bubbling elsewhere
+        }             
+    }
     function writeScript(url) {  // スクリプト注入。
         var ws = createElem('script');
         ws.type = 'text/javascript';
@@ -120,24 +153,7 @@ var Calendar_Blogger = Calendar_Blogger || function() {
     function fm(m) {  // 数値を2桁の固定長にする。
         return ("0" + m).slice(-2);
     }
-    function popUpTitles(e) {
-        e=e||event; // IE sucks
-        var target = e.target||e.srcElement; // targetはaになる。// and sucks again // target is the element that has been clicked
-        if (target.firstChild) {
-            target.firstChild.textContent
-            
-            
-            
-            return false; // stop event from bubbling elsewhere
-        }        
-        
-        
-    }
-
-
-    
     return cl;
 }();
-
 Calendar_Blogger.all("calendar_blogger");
 
